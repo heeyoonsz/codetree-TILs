901d27c45ca93c5d31c281813398dec13971ff3b
yoon <62053162+heeyoonsz@users.noreply.github.com>
2024-03-07 13:29:01 +0900

    Title: 그 다음 알파벳, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240307/그 다음 알파벳/next-alphabet.cpp
+++ 240307/그 다음 알파벳/next-alphabet.cpp
@@ -7,9 +7,14 @@ int main() {
     char alpha;
 
     cin >> alpha;
+    char x;
+    
+  //  cout << (char)((int)alpha+1);
 
-    cout << (char)((int)alpha+1);
-
+    if (x == 'z')
+        cout << "a";
+    else
+        cout << (char)(alpha+1);
 
     return 0;
 }
