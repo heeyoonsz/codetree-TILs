81b89b266a854ca8777f9dba7adc4f3b04b39ebd
yoon <62053162+heeyoonsz@users.noreply.github.com>
2024-03-07 13:21:40 +0900

    Title: 아스키코드 변환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240307/아스키코드 변환/convert-to-ascii.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    char a;
+    int b;
+    
+    cin >> a >> b;
+
+    int num = (int)a;
+    char b_char = (char)b;
+
+    cout << num <<" "<< b_char;
+
+    return 0;
+}
