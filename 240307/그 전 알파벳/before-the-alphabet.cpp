c0ceb371242383a74e0f952d014a66de5256bdff
yoon <62053162+heeyoonsz@users.noreply.github.com>
2024-03-07 13:39:34 +0900

    Title: 그 전 알파벳, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240307/그 전 알파벳/before-the-alphabet.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+   
+    char a;
+    cin >> a;
+
+    if(a=='z'){
+        cout << 'a';
+    }
+    else
+        cout << (char)(a-1);
+    
+    return 0;
+}
